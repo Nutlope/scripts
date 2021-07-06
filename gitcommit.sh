fa730054d42c6b9089ee838214e9686720bc99eb
nutlope <he55@drexel.edu>
2021-07-06 17:48:35 -0400

    small change in commit script

--- gitcommit.sh
+++ gitcommit.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Script to run git add, commit and push in one command. Commit message is 1st arg
+# Script to run git add, commit and push in one command. Commit message is 1st arg so you would run like so: "commit $MESSAGE"
 
 git add .
 git commit -m "$1"
